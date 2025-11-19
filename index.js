ba0c8623fc1856afba62bcce0d498739c445d17a
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 18:35:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function calculateAward(points) {
   if (points > 99) {
     return document.getElementById('result').innerHTML = 'First Place';
-  } else If(points>49 && points<100) {
+  } else { If(points>49 && points<100) {
     document.getElementById('result').innerHTML = 'Second Place';
   } else {
     document.getElementById('result').innerHTML = 'Participation Award';
-  };
+  }};
 };
+
